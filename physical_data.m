c8b6b8fee0a1afc0b16f6b42d1d557b77b1f16a2
David955 <37373827+David955@users.noreply.github.com>
2020-04-25 00:25:29 +0200

    Robot links ready for simscape, creation of physical data script
    1. Finished robot links in .ipt format, ready to be placed in "file_solid" simscape block
    2. Creation of physical_data.m which fills workspace with data about links

---
+++ physical_data.m
@@ -0,0 +1,51 @@
+%% servo JX PDI-HV5932MG-180    %
+
+SERVO_weight = 0.059;    % [kg]
+SERVO_length = 54.2;     % [mm]
+SERVO_width = 20;        % [mm]
+SERVO_heigth_1 = 38;     % [mm] 
+SERVO_height_2 = 44;     % [mm] height with rotational element
+
+%% servo arm    %
+
+SA_weight = 0.002;   % [kg]
+
+%% Link1    %
+
+LINK1_weight = 0;   % [kg]
+LINK1_height = 0;   % [mm]
+LINK1_width = 0;    % [mm]
+LINK1_length = 0;   % [mm]
+LINK1_com = [0 0 0]; 
+
+%% Link2    %
+
+LINK2_weight = 0;   % [kg]
+LINK2_height = 0;   % [mm]
+LINK2_width = 0;    % [mm]
+LINK2_length = 0;   % [mm]
+LINK2_com = [0 0 0];
+
+%% Link3    %
+
+LINK3_weight = 0;   % [kg]
+LINK3_height = 0;   % [mm]
+LINK3_width = 0;    % [mm]
+LINK3_length = 0;   % [mm]
+LINK3_com = [0 0 0];
+
+%% Link4    %
+
+LINK4_weight = 0;   % [kg]
+LINK4_height = 0;   % [mm]
+LINK4_width = 0;    % [mm]
+LINK4_length = 0;   % [mm]
+LINK4_com = [0 0 0];
+
+%% Effector    %
+
+EFFECTOR_weight = 0;   % [kg]
+EFFECTOR_height = 0;   % [mm]
+EFFECTOR_width = 0;    % [mm]
+EFFECTOR_length = 0;   % [mm]
+EFFECTOR_com = [0 0 0];
